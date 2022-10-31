947b93fc729629506975241b9486b81292cc4db8
NourhanE.Hassan <92875566+nouribrahem@users.noreply.github.com>
2022-10-31 21:21:41 +0200

    edited header

--- BigReal.h
+++ BigReal.h
@@ -11,30 +11,30 @@ using namespace std;
 class BigReal
 {
 private:
-    string beforePoint, afterPoint;
+        string beforePoint, afterPoint;
 	int beforePointSize, afterPointSize;
 	char numSign;
 protected:
-    bool isValidStrNum(const string& num);
+	bool isValidStrNum(const string& num);
 	BigReal addTwoStrNums(const BigReal& anotherDec);
-    BigReal removeZeroes(BigReal& number);
+	BigReal removeZeroes(BigReal& number);
 public:
-    BigReal (double realNumber = 0.0); // Default constructor
-    BigReal (const string& realNumber);
-    BigReal (const BigDecimalInt& bigInteger);
-    BigReal (const BigReal& other);  // Copy constructor
-    BigReal (BigReal&& other);   // Move constructor
-    BigReal& operator= (const BigReal& other); // Assignment operator
-    BigReal& operator= (BigReal&& other); // Move assignment
-    BigReal operator+ (const BigReal& other);
-    BigReal operator- (const BigReal& other);
-    bool operator<  (const BigReal& anotherReal);
-    bool operator>  (const BigReal& anotherReal);
-    bool operator== (const BigReal& anotherReal);
-    int size();
-    int sign();
-    friend ostream& operator << (ostream& out, const BigReal& num);
-    friend istream& operator >> (istream& out, const BigReal& num);
+	BigReal (double realNumber = 0.0); // Default constructor
+	BigReal (const string& realNumber);
+	BigReal (const BigDecimalInt& bigInteger);
+	BigReal (const BigReal& other);  // Copy constructor
+	BigReal (BigReal&& other);   // Move constructor
+	BigReal& operator= (const BigReal& other); // Assignment operator
+	BigReal& operator= (BigReal&& other); // Move assignment
+	BigReal operator+ (const BigReal& other);
+	BigReal operator- (const BigReal& other);
+	bool operator<  (const BigReal& anotherReal);
+	bool operator>  (const BigReal& anotherReal);
+	bool operator== (const BigReal& anotherReal);
+	int size();
+	int sign();
+	friend ostream& operator << (ostream& out, const BigReal& num);
+	friend istream& operator >> (istream& out, const BigReal& num);
 };
 
 
