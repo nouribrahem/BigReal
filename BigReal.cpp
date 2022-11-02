5f12fe9c5ea9786aa5dc1ba4c6cb137fbe69e9c4
NourhanE.Hassan <92875566+nouribrahem@users.noreply.github.com>
2022-11-02 23:47:49 +0200

    edited fol7 version of constructors -_-

--- BigReal.cpp
+++ BigReal.cpp
@@ -5,7 +5,6 @@
 //int beforePointSize, afterPointSize;
 //char numSign;
 
-using namespace std;
 BigReal :: BigReal (double realNumber){
     string strReal = to_string(realNumber);
     int index = strReal.find('.');
@@ -13,39 +12,34 @@ BigReal :: BigReal (double realNumber){
         numSign = strReal[0];
         beforePoint = strReal.substr(1,index);
         afterPoint = strReal.substr(index+1);
+        beforePointSize = strReal.substr(1,index).size();
+        afterPointSize = strReal.substr(index+1).size();
     }
     else{
         numSign ='+';
         beforePoint = strReal.substr(0,index);
         afterPoint = strReal.substr(index+1);
+        beforePointSize = strReal.substr(0,index).size();
+        afterPointSize = strReal.substr(index+1).size();
     }
     cout << numSign << beforePoint << '.'<< afterPoint<<'\n';
 
 }
 BigReal :: BigReal (const string& realNumber){
     int index = realNumber.find('.');
-
     if (realNumber[0]== '+' || realNumber[0] =='-'){
         numSign = realNumber[0];
         beforePoint = realNumber.substr(1,index-1);
         afterPoint = realNumber.substr(index+1);
+        beforePointSize = realNumber.substr(1,index-1).size();
+        afterPointSize = realNumber.substr(index+1).size();
     }
     else{
         numSign ='+';
-        beforePoint = realNumber.substr(0,index-1);
+        beforePoint = realNumber.substr(0,index);
         afterPoint = realNumber.substr(index+1);
+        beforePointSize = realNumber.substr(0,index).size();
+        afterPointSize = realNumber.substr(index+1).size();
     }
-    cout << numSign << beforePoint << '.'<< afterPoint<<'\n';
-}
-BigReal :: BigReal (BigDecimalInt& bigInteger){
-    afterPoint = "0";
-    beforePoint = bigInteger.getNumber();
-    if(bigInteger.sign()){
-        numSign='+';
-    }
-    else {
-        numSign = '-';
-    }
-    cout << numSign << beforePoint << '.'<< afterPoint<<'\n';
-
+    cout << numSign << beforePoint << '.'<< afterPoint<< '\n';
 }
